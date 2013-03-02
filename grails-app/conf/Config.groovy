e1c6c1ec4e278604af07b17f3de902d3fbf6aee0
Rafael <rv@manoderecha.mx>
2013-03-01 21:01:03 -0600

    Configuración básica del plugin de mail. Cambiar por datos reales.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -69,6 +69,19 @@ environments {
     }
 }
 
+grails {
+	mail {
+	  host = "smtp.gmail.com"
+	  port = 465
+	  username = "youracount@gmail.com"
+	  password = "yourpassword"
+	  props = ["mail.smtp.auth":"true",
+			   "mail.smtp.socketFactory.port":"465",
+			   "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
+			   "mail.smtp.socketFactory.fallback":"false"]
+	}
+}
+
 bruteforcedefender {
 	time = 5
 	allowedNumberOfAttempts = 3
